337c407728994ab7dd1213c6ce702e1f55403a5a
Safaa Duridi <mghanem@360moms.net>
2025-03-06 16:17:10 +0200

    changes due user2 page

---
+++ assets/javascript/users2.js
@@ -0,0 +1,348 @@
+import { apiFetch, apiPostOrPut } from './apiHelper.js';
+const baseUrl = 'http://127.0.0.1:8000/api/';
+const serverUrl = 'http://147.93.63.182/api/';
+const accessToken = localStorage.getItem('accessToken');
+const role = localStorage.getItem('role');
+if(!accessToken || accessToken == null || role == 0){
+    window.location.href = 'sign-in.html';
+}else {
+    fetchCustomers();
+}
+function fetchCustomers(){
+    const apiUrl = serverUrl + `users`;
+    apiFetch(apiUrl).then(data => { 
+        console.log(data.users);
+        fetchUsersDropDown(data.users);
+        displayCustomers(data.users);
+        
+    }).catch(error => {
+        console.error('Error fetching users:', error);
+    });
+}
+
+function displayCustomers(customers) {
+    const customerList = document.getElementById('customerList');
+    customerList.innerHTML = '';
+    customers.forEach(customer => {
+        const customerItem = document.createElement('div');
+        customerItem.classList.add('customer-item');
+        customerItem.innerHTML = `
+            
+            <img src="assets/img/team-3.jpg" alt="profile">
+            <div>
+                <a href="#"> <div>${customer.name}</div> </a>
+                <small>${customer.phone}</small>
+            </div>
+        `;
+        const link = customerItem.querySelector('a');
+        link.dataset.id = customer.id;
+        link.dataset.name = customer.name;
+        link.dataset.phone = customer.phone;
+        link.dataset.address = customer.address;
+        link.dataset.city = customer.city;
+        link.dataset.containers_count = customer.containers_count;
+        link.addEventListener('click', function (event) {
+            event.preventDefault();  // Prevent any accidental navigation
+            fetchUploadedFiles(this, customer.id);
+            displayCustomerInfo(this);  // 'this' will correctly refer to <a>
+            
+        });
+
+        customerList.appendChild(customerItem);
+
+
+    });
+}
+function fetchUsersDropDown(customers){
+    console.log("Fetching users... into dropdown");
+    const userList = document.getElementById('userList');
+    userList.innerHTML = '';
+    customers.forEach(customer => {
+        const option = document.createElement('option');
+        option.value = customer.id;
+        option.text = customer.name;
+        userList.add(option);
+    });
+}
+window.displayCustomerInfo = function (element) {
+    const customerId = element.dataset.id;
+    const customerName = element.dataset.name;
+    const customerPhone = element.dataset.phone;
+    const customerAddress = element.dataset.address;
+    const customerCity = element.dataset.city;
+    const containersCount = element.dataset.containers_count;
+    
+    document.getElementById('customerName').innerHTML = "<strong>الاسم:</strong> " + customerName;
+    document.getElementById('customerPhone').innerHTML = "<strong>الجوال:</strong> " + customerPhone;
+    document.getElementById('customerCity').innerHTML = "<strong>المدينة:</strong> " + customerCity;
+    document.getElementById('customerAddress').innerHTML = "<strong>العنوان:</strong> " + customerAddress;
+    document.getElementById('containersCount').innerHTML = "<strong>عدد الحاويات:</strong> " + containersCount;
+    document.getElementById('customerId').value = customerId;
+
+    document.getElementById('changePassword').disabled = false;
+    document.getElementById('editCustomer').disabled = false;
+    document.getElementById('deleteCustomer').disabled = false;
+
+}
+
+window.openChangePasswordModal = function () {
+    const restPasswordModal = new bootstrap.Modal(document.getElementById('restPasswordModal'));
+    restPasswordModal.show();
+ }
+
+
+ const restPasswordBTN = document.getElementById('restPasswordBTN');
+ restPasswordBTN.addEventListener('click', async  function() {
+     const newPassword = document.getElementById('newPassword').value;
+     const userIdRestPassword = document.getElementById('customerId').value;
+     if(userIdRestPassword == '' || userIdRestPassword == null || userIdRestPassword == undefined) {
+        Swal.fire('قم باختيار زبون اولا !');
+        return;
+     }
+     const apiUrl = serverUrl + `users/${userIdRestPassword}/reset-password`;
+     if(newPassword == '') {
+         Swal.fire('الرجاء ادخال كلمة المرور الجديدة');
+         return;
+     }
+     const body = {
+         password: newPassword
+     };
+         try {
+             const response = await apiPostOrPut(apiUrl, 'PATCH', body);
+             document.getElementById('newPassword').value = '';
+             document.getElementById('userIdRestPassword').value = '';
+             Swal.fire("تم التغيير بنجاح", "تم تغيير كلمة المرور بنجاح", "success");
+         } catch (error) {
+             console.error('Error :', error);
+             Swal.fire('Saver issue, contact support');
+         }
+ 
+ });
+
+ window.deleteCustomer = function () {
+     Swal.fire({
+         title: "هل انت متاكد من حذف الحساب ؟!",
+         showDenyButton: true,
+          confirmButtonText: "أحذف !",
+         denyButtonText: `لا تحذف !`
+       }).then(async (result) => {
+         if (result.isConfirmed) {
+             const id = document.getElementById('customerId').value;
+             if(id == '' || id == null || id == undefined) {
+                Swal.fire('قم باختيار زبون اولا !');
+                return;
+             }
+             const apiUrl = serverUrl + `users/${id}`;
+             try {
+                 const response = await apiPostOrPut(apiUrl, 'DELETE', {});
+                 console.log(response);
+             } catch (error) {
+                 console.error('Error :', error);
+                 alert('Saver issue, contact support');
+             }
+             Swal.fire({
+                 position: "center",
+                 icon: "success",
+                 title: "تم حذف الزبون بنجاح",
+                 showConfirmButton: false,
+                 timer: 1500
+             });
+             fetchCustomers();
+             restCustomerInfo();
+         } else if (result.isDenied) {
+           Swal.fire("لم يتم حذف الزبون", "", "info");
+         }
+       });
+ }
+ function restCustomerInfo(){
+    document.getElementById('changePassword').disabled = true;
+    document.getElementById('editCustomer').disabled = true;
+    document.getElementById('deleteCustomer').disabled = true;
+    document.getElementById('customerName').innerHTML = "<strong>الاسم:</strong> " + '-';
+    document.getElementById('customerPhone').innerHTML = "<strong>الجوال:</strong> " + '-';
+    document.getElementById('customerCity').innerHTML = "<strong>المدينة:</strong> " + '-';
+    document.getElementById('customerAddress').innerHTML = "<strong>العنوان:</strong> " + '-';
+    document.getElementById('containersCount').innerHTML = "<strong>عدد الحاويات:</strong> " + '-';
+    document.getElementById('customerId').value = '';
+ }
+
+ window.fetchUploadedFiles = async function (element,userId) {
+     console.log("Fetching files for user ID:", userId);
+     try {
+         const apiUrl = serverUrl + "users/" + userId + "/uploaded-files";
+         apiFetch(apiUrl).then(data => { 
+             console.log(data.files);
+             displayFiles(data.files);
+         }).catch(error => {
+             console.error('Error fetching users:', error);
+         });
+ 
+     } catch (error) {
+         console.error("Error fetching files:", error);
+     }
+ }
+
+ function displayFiles(files) {
+    const containerFilesDiv = document.getElementById("containerFiles2"); // Updated ID
+    containerFilesDiv.innerHTML = "";
+
+    files.forEach(file => {
+        const fileElement = createFileElement(file);
+        if (file.type === "container") {
+            containerFilesDiv.appendChild(fileElement);
+        } // TODO : set the bank statment file
+        
+    });
+}
+
+function createFileElement(file) {
+    const fileDiv = document.createElement("div");
+    fileDiv.classList.add("file-item");
+    const dateSpan = document.createElement("span");
+    const timestamp = file.date;
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+    dateSpan.textContent = formattedDate; 
+
+    const nameSpan = document.createElement("span");
+    nameSpan.textContent = file.name;
+
+    const fileIcon = document.createElement("img");
+    fileIcon.classList.add("file-icon");
+    fileIcon.src = getFileIcon(file.extension);  // Use your existing icon logic
+    fileIcon.alt = file.extension;
+
+    fileDiv.appendChild(dateSpan);
+    fileDiv.appendChild(nameSpan);
+    fileDiv.appendChild(fileIcon);
+
+    // Optional: If you want to make the whole item clickable to download/view the file
+    fileDiv.style.cursor = "pointer";
+    fileDiv.addEventListener("click", () => {
+        window.open(file.url, "_blank");
+    });
+
+    return fileDiv;
+}
+
+function getFileIcon(extension) {
+    const fileIcons = {
+        "pdf": "https://cdn-icons-png.flaticon.com/512/337/337946.png",
+        "doc": "https://cdn-icons-png.flaticon.com/512/732/732220.png",
+        "docx": "https://cdn-icons-png.flaticon.com/512/732/732220.png",
+        "xls": "https://cdn-icons-png.flaticon.com/512/732/732220.png",
+        "xlsx": "https://cdn-icons-png.flaticon.com/512/732/732220.png",
+        "png": "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        "jpg": "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        "jpeg": "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        "gif": "https://cdn-icons-png.flaticon.com/512/732/732212.png"
+    };
+    return fileIcons[extension] || "https://cdn-icons-png.flaticon.com/512/564/564619.png";
+}
+
+const createCustomerBTN = document.getElementById('createCustomerBTN');
+createCustomerBTN.addEventListener('click', async  function() {
+    console.log("Create Customer Button Clicked");
+    const apiUrl = serverUrl + 'users';
+    const name = document.getElementById('customerNameCreate').value;
+    const email = document.getElementById('customerEmailCreate').value;
+    const phone = document.getElementById('customerPhoneCreate').value;
+    const password = document.getElementById('customerPasswordCreate').value;
+    const address = document.getElementById('customerAddressCreate').value;
+    const city = document.getElementById('customerCityCreate').value;
+    if(name == '' || email == '' || phone == '' || password == '' || address == '' || city == '') {
+        Swal.fire('الرجاء ادخال جميع البيانات');
+        return;
+    }
+    const body = {
+        name: name,
+        email: email,
+        phone: phone,
+        password: password,
+        address: address,
+        city: city
+    };
+    try {
+        const response = await apiPostOrPut(apiUrl, 'POST', body);
+        console.log(response);
+        document.getElementById('customerNameCreate').value = '';
+        document.getElementById('customerEmailCreate').value = '';
+        document.getElementById('customerPhoneCreate').value = '';
+        document.getElementById('customerPasswordCreate').value = '';
+        document.getElementById('customerAddressCreate').value = '';
+        document.getElementById('customerCityCreate').value = '';
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "تم انشاء الزبون بنجاح",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        document.getElementById('closeCreateModal').click();
+        fetchCustomers();
+    } catch (error) {
+        console.error('Error :', error);
+        alert('Saver issue, contact support');
+    }
+
+});
+
+const sendNoficationBTN = document.getElementById('sendNoficationBTN');
+sendNoficationBTN.addEventListener('click', async  function() {
+    console.log("sendNoficationBTN clicked");
+    const subject = document.getElementById('notificationSubject').value;
+    const message = document.getElementById('notificationBody').value;
+    const notificationType = document.querySelector('input[name="notificationType"]:checked').value;
+    let userIds = [];
+    if (notificationType === 'C') {
+        const selectedOptions = document.getElementById('userList').selectedOptions;
+        userIds = Array.from(selectedOptions).map(option => option.value);
+    }
+
+    // Validate required fields
+    if (!subject || !message) {
+        Swal.fire('الرجاء إدخال الموضوع والمحتوى');
+        return;
+    }
+
+    if (notificationType === 'C' && userIds.length === 0) {
+        Swal.fire('الرجاء اختيار الزبائن');
+        return;
+    }
+    const body = {
+        subject,
+        message,
+        notificationType,
+        user_ids: notificationType === 'C' ? userIds : []  // Send user_ids only if type C is selected
+    };
+
+    try {
+        const apiUrl = serverUrl + 'user/notifications/send';
+        const response = await apiPostOrPut(apiUrl, 'POST', body);
+        document.getElementById('notificationSubject').value = '';
+        document.getElementById('notificationBody').value = '';
+        // document.getElementById('userList').selectedOptions.forEach((option) => {
+        //     option.selected = false;
+        // });
+        document.getElementById('userSelection').classList.add('d-none');
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "تم إرسال الإشعار بنجاح",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        Swal.fire('خطأ في الإرسال، الرجاء المحاولة مرة أخرى');
+    }
+
+});
